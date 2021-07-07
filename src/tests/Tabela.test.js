922cd372474dd76839d37b6982e354d47b816902
Daniela Palumbo <daniela.palumbo@outlook.com>
2021-07-07 10:25:15 -0300

    Create new tabela test

--- src/tests/Tabela.test.js
+++ src/tests/Tabela.test.js
@@ -4,7 +4,26 @@ import { screen, render } from '@testing-library/react';
 import Tabela from '../components/Tabela';
 
 describe('Tabela', () => {
-  it('Deve renderizar o componente', () => {
+  it('Deve renderizar o componente sem colunas', () => {
+    render(<Tabela 
+      linhas={[]}
+      colunas={[]}
+      addItem={()=>{}}
+      updateItem={()=>{}}
+      deleteItem={()=>{}}
+      selectedItem={null}
+      rowSelectedFunction={()=>{}}
+      />);
+
+    const excluirBtn = screen.getByTestId('btn-excluir');
+    const alterarBtn = screen.getByTestId('btn-alterar');
+    const cadastroBtn = screen.getByTestId('btn-cadastrar');
+    
+    expect(cadastroBtn).toBeInTheDocument();
+    expect(excluirBtn).toBeInTheDocument();
+    expect(alterarBtn).toBeInTheDocument();
+  });
+  it('Deve renderizar o componente com colunas', () => {
     const colunas = [{ field: 'nome', headerName: 'Marca', width: 200 }];
     render(<Tabela 
       linhas={[]}
@@ -19,7 +38,8 @@ describe('Tabela', () => {
     const excluirBtn = screen.getByTestId('btn-excluir');
     const alterarBtn = screen.getByTestId('btn-alterar');
     const cadastroBtn = screen.getByTestId('btn-cadastrar');
-
+    
+    expect(screen.getByText('Marca')).toBeInTheDocument();
     expect(cadastroBtn).toBeInTheDocument();
     expect(excluirBtn).toBeInTheDocument();
     expect(alterarBtn).toBeInTheDocument();
