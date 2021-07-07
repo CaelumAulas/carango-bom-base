2d74cc9cf1c44b08e9bd108b4a1518ac25eadfc7
Daniela Palumbo <daniela.palumbo@outlook.com>
2021-07-07 14:45:35 -0300

    Full coverage on Tabela

--- src/tests/Tabela.test.js
+++ src/tests/Tabela.test.js
@@ -1,47 +1,45 @@
-import React from 'react';
-import { screen, render } from '@testing-library/react';
+import React from "react";
+import { screen, render, act } from "@testing-library/react";
+import Tabela from "../components/Tabela";
+import userEvent from '@testing-library/user-event'
 
-import Tabela from '../components/Tabela';
+const brandsMock = [{ id: 0, nome: "Fiat" }];
+const colunas = [{ field: "nome", headerName: "Marca", width: 200 }];
+const mockFun = jest.fn();
 
-describe('Tabela', () => {
-  it('Deve renderizar o componente sem colunas', () => {
-    render(<Tabela 
-      linhas={[]}
-      colunas={[]}
-      addItem={()=>{}}
-      updateItem={()=>{}}
-      deleteItem={()=>{}}
-      selectedItem={null}
-      rowSelectedFunction={()=>{}}
-      />);
-
-    const excluirBtn = screen.getByTestId('btn-excluir');
-    const alterarBtn = screen.getByTestId('btn-alterar');
-    const cadastroBtn = screen.getByTestId('btn-cadastrar');
-    
-    expect(cadastroBtn).toBeInTheDocument();
-    expect(excluirBtn).toBeInTheDocument();
-    expect(alterarBtn).toBeInTheDocument();
-  });
-  it('Deve renderizar o componente com colunas', () => {
-    const colunas = [{ field: 'nome', headerName: 'Marca', width: 200 }];
-    render(<Tabela 
-      linhas={[]}
+const setup = () =>
+  render(
+    <Tabela
+      linhas={brandsMock}
       colunas={colunas}
-      addItem={()=>{}}
-      updateItem={()=>{}}
-      deleteItem={()=>{}}
+      addItem={() => {}}
+      updateItem={() => {}}
+      deleteItem={() => {}}
       selectedItem={null}
-      rowSelectedFunction={()=>{}}
-      />);
+      rowSelectedFunction={mockFun}
+    />
+  );
 
-    const excluirBtn = screen.getByTestId('btn-excluir');
-    const alterarBtn = screen.getByTestId('btn-alterar');
-    const cadastroBtn = screen.getByTestId('btn-cadastrar');
-    
-    expect(screen.getByText('Marca')).toBeInTheDocument();
+beforeEach(async () => {
+  await act(async () => setup());
+});
+
+describe("Tabela", () => {
+  it("Deve renderizar o componente com colunas", () => {
+    const excluirBtn = screen.getByTestId("btn-excluir");
+    const alterarBtn = screen.getByTestId("btn-alterar");
+    const cadastroBtn = screen.getByTestId("btn-cadastrar");
+
+    expect(screen.getByText("Marca")).toBeInTheDocument();
     expect(cadastroBtn).toBeInTheDocument();
     expect(excluirBtn).toBeInTheDocument();
     expect(alterarBtn).toBeInTheDocument();
   });
+  it("Deve renderizar o componente com linhas", () => {
+    expect(screen.getByText("Fiat")).toBeInTheDocument();
+  });
+  it('Deve chamar a função rowSelect ao ser selecionada', ()=>{
+     userEvent.click(screen.getByText('Fiat')) 
+      expect(mockFun).toHaveBeenCalled();
+  })
 });
