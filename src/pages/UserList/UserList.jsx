cf2a3f6b6493c1ad5e3c3eed37eb7a7425b0dedb
Daniel Batista <danielbatista11@outlook.com>
2021-07-09 12:44:11 -0300

    Fix sonar bug

--- src/pages/UserList/UserList.jsx
+++ src/pages/UserList/UserList.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router";
-import UserService from "../../../services/UserService";
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import UserService from '../../../services/UserService';
 
-import Table from "../../../components/Table/Table";
+import Table from '../../../components/Table/Table';
 
-const columns = [{ field: "name", headerName: "Usuario", width: 200 }];
+const columns = [{ field: 'name', headerName: 'Usuario', width: 200 }];
 
 export default function UserList() {
   const [users, setUsers] = useState([]);
-  const [selectedUser, setSelectedUser] = useState();
+  const [selectedUser, setSelectedUser] = useState(null);
   const history = useHistory();
 
   function create() {
-    history.push("/cadastro-usuario");
+    history.push('/cadastro-usuario');
   }
 
   function update() {
-    history.push("/alteracao-usuario/" + selectedUser.id);
+    history.push('/alteracao-usuario/' + selectedUser.id);
   }
 
   function deleteUser() {
@@ -33,7 +33,7 @@ export default function UserList() {
   }
 
   return (
-    <div style={{ height: 300, width: "100%" }}>
+    <div style={{ height: 300, width: '100%' }}>
       <Table
         linhas={users}
         colunas={columns}
