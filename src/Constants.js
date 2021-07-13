10713cdab513afdbc30dc176202a3699f636ea12
Daniela Palumbo <daniela.palumbo@outlook.com>
2021-07-13 17:09:28 -0300

    Api url constant

--- src/Constants.js
+++ src/Constants.js
@@ -1,1 +1,2 @@
 export const DRAWER_WIDTH = 240;
+export const API_URL = 'https://carango-bom-api.herokuapp.com'
--- src/services/UsuarioService.js
+++ src/services/UsuarioService.js
@@ -1,28 +1,30 @@
+import { API_URL } from '../Constants'
+
 const UsuarioService = {
     cadastrar(usuarios) {
-        return fetch('https://carango-bom-api.herokuapp.com/usuarios', {
+        return fetch(API_URL+'/usuarios', {
           method: 'POST',
           body: JSON.stringify(usuarios)
         }).then(r => r.json());
       },
     
       alterar(usuarios) {
-        return fetch('https://carango-bom-api.herokuapp.com/usuarios/' + usuarios.id, {
+        return fetch(API_URL+'/usuarios/' + usuarios.id, {
           method: 'PUT',
           body: JSON.stringify(usuarios)
         }).then(r => r.json());
       },
     
       consultar(id) {
-        return fetch('https://carango-bom-api.herokuapp.com/usuarios/' + id).then(r => r.json());
+        return fetch(API_URL+'/usuarios/' + id).then(r => r.json());
       },
     
       listar() {
-        return fetch('https://carango-bom-api.herokuapp.com/usuarios').then(r => r.json());
+        return fetch(API_URL+'/usuarios').then(r => r.json());
       },
     
       excluir(usuarios) {
-        return fetch('https://carango-bom-api.herokuapp.com/usuarios/' + usuarios.id, {
+        return fetch(API_URL+'/usuarios/' + usuarios.id, {
           method: 'DELETE',
         })
           .then(r => r.json());
